5b8f48954734fbc630810162506115557ba63fbe
Sergio Laptop <sergo9292@gmail.com>
2022-09-20 05:48:12 -0500

    new files created and code added

---
+++ s3.tf
@@ -0,0 +1,6 @@
+resource "aws_s3_bucket" "sergio-s3-resource" {
+  bucket = "sergios-terraform-bucket"
+}
+resource "aws_s3_bucket" "sergio-s3-resource-2" {
+  bucket = "sergios-terraform-bucket-2"
+}
