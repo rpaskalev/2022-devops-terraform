e00e43dfc0a6ab81adc03a2ef68e878d7f9a0bb8
Sergio Laptop <sergo9292@gmail.com>
2022-09-20 07:35:28 -0500

    s3 ready for static website hosting

--- s3.tf
+++ s3.tf
@@ -1,3 +1,47 @@
-resource "aws_s3_bucket" "devops_s3_course_2022" {
-  bucket = "rady-bucket-unique-name"
+variable "site-name" {
+    type = string
+    default = "your-unique-website.com"
+}
+variable "my-name" {
+    type = string
+    default = "Sergio"
+}
+
+resource "aws_s3_bucket" "website-bucket" {
+    bucket = var.site-name
+    tags = {
+        Name = var.my-name
+    }
+}
+
+resource "aws_s3_bucket_versioning" "web-bucket-v" {
+    bucket = aws_s3_bucket.website-bucket.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_website_configuration" "website-config" {
+    bucket = aws_s3_bucket.website-bucket.id
+
+    index_document {
+        suffix = "index.html"
+    }
+}
+resource "aws_s3_bucket_policy" "website-bucket-policy" {
+    bucket = aws_s3_bucket.website-bucket.id
+    policy = <<EOT
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "${aws_s3_bucket.website-bucket.arn}/*"
+        }
+    ]
+    }
+EOT
 }
