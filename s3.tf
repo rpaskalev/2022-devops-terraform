140e80c9442be2ce7acf10576d6bf62fab8aab4c
ismai <ismailmouharir@gmail.com>
2022-09-21 13:05:52 -0400

    ismail's changes

--- s3.tf
+++ s3.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "devops_s3_course_2022" {
   bucket = "rady-bucket-unique-name"
+  versioning {
+    enabled = true
+
+    tags = {
+    Name        = "ismail"
+    }
+  }   
 }
+
