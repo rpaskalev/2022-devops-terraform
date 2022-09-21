4b4c1da1f3a515a066749817f050f15afbd94591
Saidmakh09 <saidmahmud2003@gmail.com>
2022-09-21 17:30:45 -0500

    changed

--- s3.tf
+++ s3.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "devops_s3_course_2022" {
   bucket = "rady-bucket-unique-name"
+tags = {
+    Name        = "Said bucket"
+    Environment = "Dev"
+  }
+versioning {
+    enabled = true
+  }
 }
