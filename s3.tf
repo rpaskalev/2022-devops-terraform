a9758ecdac5dd17c5552a41d09d4be843fb4547c
cem <cemsevinc6@gmail.com>
2022-09-22 13:35:33 -0400

    cem_homework1

--- s3.tf
+++ s3.tf
@@ -1,3 +1,14 @@
 resource "aws_s3_bucket" "devops_s3_course_2022" {
   bucket = "rady-bucket-unique-name"
+
+  versioning {
+    enabled = true
+  }
+
+
+  tags = {
+    Name        = "cem_bucket2"
+    Environment = "dev_1"
+  }
+
 }
