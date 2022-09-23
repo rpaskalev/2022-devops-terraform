5153bea6893b8251c0b1efca63fa558177cb5a82
Sergio Laptop <sergo9292@gmail.com>
2022-09-22 21:52:28 -0500

    corrections

--- s3.tf
+++ s3.tf
@@ -1,13 +1,12 @@
-resource "aws_s3_bucket" "devops_s3_course_2022" {
+resource "aws_s3_bucket" "devops_s3_course_2022_moved_resource" {
   bucket = "rady-bucket-unique-name"
+
   tags = {
-    Name = "Sergio"
-    Type = "Shared"
+    Environment = "devops_2022"
+    Name        = "dzmitry-tr-s3"
   }
-}
-resource "aws_s3_bucket_versioning" "bucket-versioning" {
-  bucket = aws_s3_bucket.devops_s3_course_2022.id
-  versioning_configuration {
-    status = "Enabled"
+
+  versioning {
+    enabled = true
   }
 }
